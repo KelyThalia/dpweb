9d2f2ad8f5c113990fc1afac5ed2d7f35e0336f0
KelyThalia <kelyquispe2006@gmail.com>
2025-07-08 12:57:48 -0500

    345667

--- view/new-user.php
+++ view/new-user.php
@@ -1,69 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kely</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
-    <script>
-        const base_url = '<?php echo BASE_URL; ?>';
-    </script>
-</head>
-
-<body>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">clients</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">schops</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">sales</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">perfil</a></li>
-                                <li><a class="dropdown-item" href="#">logout</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">conted</a></li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <!-- INICIO DE CUERPO DE PÁGINA-->
     <div class="container_fluid">
         <div class="card">
             <h5 class="btn btn-primary">titulo</h5>
@@ -155,11 +91,4 @@
         </form>
     </div>
     </div>
-</body>
-<script src="<?php echo BASE_URL ?>view/function/user.js"></script>
-
-<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-</html>
+     <!-- FIN DE CUERPO DE PÁGINA-->
