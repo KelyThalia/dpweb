cf12c8a98d443efba2cdd81aee3f872b662e406f
KelyThalia <kelyquispe2006@gmail.com>
2025-10-13 13:05:59 -0500

    Update ProductoModel.php

--- model/ProductoModel.php
+++ model/ProductoModel.php
@@ -1,45 +1,36 @@
 <?php
 require_once("../library/conexion.php");
-class ProductoModel
-{
+class ProductoModel{
     private $conexion;
-    function __construct()
-    {
+    function __construct(){
         $this->conexion = new Conexion();
         $this->conexion = $this->conexion->connect();
-        
     }
-     
-    
-    public function registrar($codigo, $nombre, $detalle, $precio, $stock, $id_categoria, $fecha_vencimiento, $imagen, $id_proveedor)
-    {
-        $consulta = "INSERT INTO producto (codigo, nombre, detalle, precio, stock, id_categoria, fecha_vencimiento, imagen, id_proveedor) 
-        VALUES('$codigo', '$nombre', '$detalle', '$precio', '$stock', '$id_categoria', '$fecha_vencimiento', '$imagen', '$id_proveedor')";
+    public function registrar($codigo, $nombre, $detalle, $precio, $stock, $id_categoria, $fecha_vencimiento) {
+        $consulta = "INSERT INTO producto (codigo, nombre, detalle, precio, stock, id_categoria, fecha_vencimiento) VALUE('$codigo', '$nombre', '$detalle', '$precio', '$stock', '$id_categoria', '$fecha_vencimiento')";
         $sql = $this->conexion->query($consulta);
         if ($sql) {
             $sql = $this->conexion->insert_id;
-        } else {
+        }else{
             $sql = 0;
         }
         return $sql;
     }
 
-    public function existeProducto($codigo)
-    {
+    public function existeProducto($codigo){
         $consulta = "SELECT * FROM producto WHERE codigo ='$codigo'";
         $sql = $this->conexion->query($consulta);
         return $sql->num_rows;
+
     }
 
-    public function buscarProductoPorCodigo($codigo)
-    {
+    public function buscarProductoPorCodigo($codigo){
         $consulta = "SELECT id, codigo FROM producto WHERE codigo='$codigo' LIMIT 1";
         $sql = $this->conexion->query($consulta);
         return $sql->fetch_object();
     }
 
-    public function mostrarProductos()
-    {
+    public function mostrarProductos() {
         $arr_productos = array();
         $consulta = "SELECT * FROM producto";
         $sql = $this->conexion->query($consulta);
@@ -52,24 +43,21 @@ class ProductoModel
         }
         return $arr_productos;
     }
+    
 
-
-    public function ver($id)
-    {
+    public function ver($id){
         $consulta = "SELECT * FROM producto WHERE id = '$id'";
         $sql = $this->conexion->query($consulta);
         return $sql->fetch_object();
     }
 
-    public function actualizar($id_producto, $codigo, $nombre, $detalle, $precio, $stock, $id_categoria, $fecha_vencimiento, $imagen, $id_proveedor)
-    {
-        $consulta = "UPDATE producto SET codigo='$codigo', nombre='$nombre', detalle='$detalle', precio='$precio', stock='$stock', id_categoria='$id_categoria', fecha_vencimiento='$fecha_vencimiento', imagen='$imagen', id_proveedor='$id_proveedor' WHERE id='$id_producto'";
+    public function actualizar($id_producto, $codigo, $nombre, $detalle, $precio, $stock, $id_categoria, $fecha_vencimiento){
+        $consulta = "UPDATE producto SET codigo='$codigo', nombre='$nombre', detalle='$detalle', precio='$precio', stock='$stock', id_categoria='$id_categoria', fecha_vencimiento='$fecha_vencimiento' WHERE id='$id_producto'";
         $sql = $this->conexion->query($consulta);
         return $sql;
     }
 
-    public function eliminar($id_producto)
-    {
+    public function eliminar($id_producto){
         $consulta = "DELETE FROM producto WHERE id='$id_producto'";
         $sql = $this->conexion->query($consulta);
         return $sql;
